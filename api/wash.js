496507caffcefcaf574a2bb6e69a5e5b160b3528
liangkangnian <65379487+liangkangnian@users.noreply.github.com>
2026-03-09 23:26:37 +0800

    Reduce max_tokens to 300 in wash.js
    reduce max_tokens to speed up

--- api/wash.js
+++ api/wash.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         model: model || apiModel,
-        max_tokens: max_tokens || 2000,
+        max_tokens: max_tokens || 300,
         messages: finalMessages,
       }),
     });
